fd2fe25458f97f1a06714f8bbd7728702df6f64b
ih2502mk <ih2502mk@gmail.com>
2012-01-20 00:14:40 +0000

    rapid prototype of kinetic effet

--- js/bigbg.js
+++ js/bigbg.js
@@ -1,5 +1,7 @@
 function Vect(x, y){
-  this.setXY(x, y);
+    this.x;
+    this.y;
+    this.setXY(x, y);
 }
         
 Vect.prototype.vadd = function(v1, v2) {
@@ -23,6 +25,36 @@ Vect.prototype.setXY = function(x, y) {
   this.y = y;
 }
 
+function Movable($el){
+    this.el = $el;
+    $el.data('movable', this);
+}
+
+Movable.prototype.move = function(x, y){
+    this.el.offset({left:x, top:y});
+}
+
+function Drag($el){
+    this.active = false;
+    this.draggy = new Movable($el);
+    this.draggyToMouse = new Vect(0, 0);
+}
+
+Drag.prototype.start = function(x, y){
+    this.active = true;
+    this.draggyToMouse.setXY(this.draggy.el.offset().left - x, this.draggy.el.offset().top - y);
+}
+
+Drag.prototype.move = function(x, y){
+    if (this.active) {
+        this.draggy.move(x + this.draggyToMouse.x, y + this.draggyToMouse.y);
+    }
+}
+
+Drag.prototype.stop = function(){
+    this.active = false;
+}
+
 $(document).ready(function(){
   var $bigbg = $('#bigbg');
   
@@ -31,14 +63,13 @@ $(document).ready(function(){
   pos = new Vect(0, 0),
   relPos = new Vect(0, 0);
             
-  var measure, move, drag = false;
+  var measure, move, drag = new Drag($('#bigbg'));
             
   $(document).mousedown(function(e){
     //TODO: wrap into method with callback as a callback pass drag init which does relPos setting and drag = true
     clearInterval(move);
     
-    relPos.setXY(($bigbg.offset().left - e.pageX), ($bigbg.offset().top - e.pageY));
-    drag = true;
+    drag.start(e.pageX, e.pageY)
     
     //TODO: wrap measure into method which sets old pos and then inits measure intervaled 
     oldPos.setXY(e.pageX, e.pageY);
@@ -51,25 +82,32 @@ $(document).ready(function(){
   });
             
   $(document).mouseup(function(e){
-    drag = false;
+    drag.stop();
     
     clearInterval(measure);
     
-    var left = $bigbg.offset().left;
-    var top = $bigbg.offset().top;
-                
+    //TODO: make this not so crappy
+    var left = drag.draggy.el.offset().left;
+    var top = drag.draggy.el.offset().top;
+    var vx = Math.abs(Math.round(speed.x/5));
+    var vy = Math.abs(Math.round(speed.y/5));
+    var dirx = vx/Math.round(speed.x/5);
+    var diry = vy/Math.round(speed.y/5);
     move = setInterval(function(){
-      left += Math.round(speed.x/10);
-      top += Math.round(speed.y/10);
-      $bigbg.offset({left:left, top:top})    
-    }, 5)
+      left += --vx*dirx;
+      top += --vy*diry;
+      if (vx && vy) {
+          drag.draggy.move(left, top);
+      }
+      else {
+          clearInterval(move);
+      }    
+    }, 10)
   });
             
   $(document).mousemove(function(e){
     pos.setXY(e.pageX, e.pageY);
     
-    if(drag) {
-      $bigbg.offset({left:(relPos.x + e.pageX), top:(relPos.y + e.pageY) })
-    }
+    drag.move(e.pageX, e.pageY);
   });
 })
